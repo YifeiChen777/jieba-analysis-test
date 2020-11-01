16997e3077c9652af2c3036b7c4c944a3752005e
Caroline Chen <yc4ag@virginia.edu>
2020-11-01 18:47:47 -0500

    stop word for small idf

--- jieba_test.py
+++ jieba_test.py
@@ -9,6 +9,11 @@ from optparse import OptionParser
 def small_corpus_idf(directory):
     documents = []
     word_set = set()
+    stop_words = []
+    with open("stopword.txt","r") as f:
+        lines = f.readlines()
+        for line in lines:
+            stop_words.append(line.rstrip('\n'))
     for filename in os.listdir(directory):
         l = []
         dic_file = os.path.join(directory, filename)
@@ -20,8 +25,11 @@ def small_corpus_idf(directory):
         word_set.update(l)
         documents.append(l)
     total_document = len(documents)
+    diction = {}
     for word in word_set:
-        if word == ' ' or word == '\n':
+        if word.isspace() :
+            continue
+        if word in stop_words:
             continue
         count = 0
         for d in documents:
@@ -29,6 +37,9 @@ def small_corpus_idf(directory):
                 count += 1
         idf = math.log(total_document / count)
         print(word + " " + str(idf))
+        diction[word] = idf
+    a = sorted(diction.items(), key=lambda x: x[1], reverse=False)
+    # print(a[0:10])
 
 
 def jieba_analyze(filename):
@@ -40,31 +51,28 @@ def jieba_analyze(filename):
     content = open(filename, 'rb').read()
     jieba.analyse.set_idf_path("toutiao_idf.txt")
     tags1 = jieba.analyse.extract_tags(content, topK=5)
-    jieba.analyse.set_idf_path("small_idf.txt")
-    tags2 = jieba.analyse.extract_tags(content, topK=5)
 
     print(",".join(tags1))
     print(",".join(tags2))
 
 if __name__ == "__main__":
-    jieba.analyse.set_stop_words("stopword.txt")
-    print("1: ")
-    jieba_analyze("static/Chi/1/1.txt")
-    print("2: ")
-    jieba_analyze("static/Chi/2/1.txt")
-    print("3: ")
-    jieba_analyze("static/Chi/3/1.txt")
-    print("Common 1: ")
-    jieba_analyze("static/Common/1/chi.txt")
-    print("Common 2: ")
-    jieba_analyze("static/Common/2/chi.txt")
-    print("Common 3: ")
-    jieba_analyze("static/Common/3/chi.txt")
-    print("Common 4: ")
-    jieba_analyze("static/Common/4/chi.txt")
-    print("Common 5: ")
-    jieba_analyze("static/Common/5/chi.txt")
-    # small_corpus_idf("static/small_corpus")
+    # print("1: ")
+    # jieba_analyze("static/Chi/1/1.txt")
+    # print("2: ")
+    # jieba_analyze("static/Chi/2/1.txt")
+    # print("3: ")
+    # jieba_analyze("static/Chi/3/1.txt")
+    # print("Common 1: ")
+    # jieba_analyze("static/Common/1/chi.txt")
+    # print("Common 2: ")
+    # jieba_analyze("static/Common/2/chi.txt")
+    # print("Common 3: ")
+    # jieba_analyze("static/Common/3/chi.txt")
+    # print("Common 4: ")
+    # jieba_analyze("static/Common/4/chi.txt")
+    # print("Common 5: ")
+    # jieba_analyze("static/Common/5/chi.txt")
+    small_corpus_idf("static/small_corpus")
     # with open("toutiao_idf.txt", 'r') as f:
     #     count = 0
     #     for line in f.readlines():
