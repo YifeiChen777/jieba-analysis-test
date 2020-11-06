be61f308fbdd03c5c36b66747e5610ec224f6a43
Caroline Chen <yc4ag@virginia.edu>
2020-11-06 00:00:29 -0500

    m

--- jieba_test.py
+++ jieba_test.py
@@ -45,7 +45,7 @@ def small_corpus_idf(directory):
 
 def jieba_analyze(filename):
     USAGE = "usage:    python extract_tags.py [file name] -k [top k]"
-    parser = OptionParser(USAGE)
+    parser = OptionParser(USAGE) #
     parser.add_option("-k", dest="topK")
     # opt, args = parser.parse_args()
 
