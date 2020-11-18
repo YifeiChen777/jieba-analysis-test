c5d446699171a43c1682ab1902439016aafe84c3
Caroline Chen <yc4ag@virginia.edu>
2020-11-18 14:49:44 -0500

    generate corpus news idf

--- generate_corpus_news.py
+++ generate_corpus_news.py
@@ -15,20 +15,20 @@ def load_json_news(filename):
     with open(filename, 'r') as f:
         flag = 0
         for line in f:
-            if flag > 1000:
-                break
-            flag += 1
+            # if flag > 1000:
+            #     break
+            # flag += 1
+            total_document += 1
             data = json.loads(line)
             jiecut = jieba.lcut(data['content'])
-            for each in jiecut:
+            for each in set(jiecut):
                 if each.isspace():
                     continue
-                total_document += 1
                 documents[each] += 1
 
-    for key, value in documents.items():
+    for word, count in documents.items():
         idf = math.log(total_document / count)
-        print(key + " " + str(idf))
+        print(word + " " + str(idf))
 
 
 if __name__ == "__main__":
