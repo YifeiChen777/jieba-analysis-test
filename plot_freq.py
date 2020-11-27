349b92b5d151c6b9b75d66d25819803a6748a3c5
Caroline Chen <yc4ag@virginia.edu>
2020-11-26 23:52:24 -0500

    update main idf

--- plot_freq.py
+++ plot_freq.py
@@ -5,3 +5,6 @@ def extract_idf(filename):
         for line in lines:
             idf.append(line.split()[1])
     return idf
+
+if __name__ == "__main__":
+    idf_lst = extract_idf("small_idf.txt")
