4e5eb8caf384456412054e5824aa3a3c58eaf327
Caroline Chen <yc4ag@virginia.edu>
2020-11-26 23:56:23 -0500

    plot function

--- plot_freq.py
+++ plot_freq.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 def extract_idf(filename):
     idf = []
     with open(filename,"r") as f:
@@ -6,5 +7,11 @@ def extract_idf(filename):
             idf.append(line.split()[1])
     return idf
 
+def plot_idf(idf_lst):
+    plt.plot(idf_lst)
+    plt.ylabel('freq')
+    plt.show()
+
 if __name__ == "__main__":
     idf_lst = extract_idf("small_idf.txt")
+    plot_idf(idf_lst)
